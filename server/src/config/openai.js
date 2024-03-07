8036760dea6b1ac6a49fd22c5ff8713cc6fc2d6c
Verônica S <veronicasgc30@gmail.com>
2024-03-07 11:59:17 -0300

    server e controller criados e configurados

---
+++ server/src/config/openai.js
@@ -0,0 +1,23 @@
+const { Configuration, OpenAIApi } = require("openai");
+
+module.exports = class openai {
+  static configuration() {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    return new OpenAIApi(configuration);
+  }
+  static textCompletion({ prompt }) {
+    return {
+      model: "gpt-3.5-turbo-instruct-0914",
+      prompt: `${prompt}`,
+      temperature: 1,
+      max_tokens: 3500,
+      top_p: 1,
+      frequency_penalty: 0.5,
+      presence_penalty: 0,
+    };
+  }
+};
+
+
